01cd1a525a45fdb98550dd592d88f2577f08d3d1
Sangeeta Thumbargudi <sangeetaprash@gmail.com>
2025-01-17 23:53:52 -0800

    changes in app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get('/')
 def home():
-    return {"message": "Hello World, from IK class enjoy it bye see you soon"}
+    return {"message": "Hello World"}
 
 if __name__ == "__main__":
     import uvicorn
